30b39faac32c1b09e8a21d5adab1787a1b9fa1ee
Ruslan Buralkiev <ruslanburalkiev8@gmail.com>
2022-10-10 18:03:38 +0600

    dz1

---
+++ main.js
@@ -0,0 +1,179 @@
+// console.log('Hello Word');
+
+//es5
+var a = 10;
+// console.log(a);
+
+//es6
+const b = 5;
+let f = 4;
+console.log(b);
+
+//Типы данных
+const number = 10//number
+console.log( typeof number);
+
+const string ='Hello'; //string//text
+// console.log(typeof string);
+
+const boolean =true;//true/false
+// console.log(typeof boolean);
+
+const object ={key: 'value'}//object
+// console.log(object)
+
+const array = [1,2,3,{name: 'Alina'}]
+// console.log(array);
+
+//object
+const obj = {
+    key: 'value',
+    name: 'Alex',
+    age: 18
+}
+// console.log(obj);
+// console.log(obj.name, obj.age);
+
+//array
+const user ={
+    login: 'Alina',
+    password: '123456',
+    username: 'Alina_228',
+    followers: [
+        {
+            username: 'Alex24',
+            descripton: 'traveler',
+        },
+        {
+            username:'oleg',
+            descripton: 'lawyer',
+        },
+    ],
+    follows: [
+        {
+            username: 'Alex24',
+            descripton: 'traveler',
+        },
+        {
+            username:'oleg',
+            descripton: 'lawyer',
+        },
+    ],
+    avatar: 'image',
+}
+
+// console.log(user);
+// console.log('username:' + user.username);
+// console.log('follows:'+ user.follows.length);
+// console.log('followers:'+ user.followers.length);
+
+
+
+
+
+
+
+//массив
+// const mass =[1,2,3 {name: 'alex'}, 'Hello', true, [1,2,3]]
+//console.log(mass);
+
+// const mass = [1,2,3,4,5];
+// console.log(mass[3]);
+// const arr = [{username: 'Alex'}, [1,2,3,4,5], 'string'];
+// console.log(arr[2]);
+
+//incriment ++
+//decrement --
+
+//операторы
+const num1 = 10;
+const num2 = 5;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// console.log(2 *(num1 + num2)-num1);
+
+//if else - если иначе
+// if(true) {
+//     // console.log('выполнится, если условие истинное(true)');
+// } else{
+//     // console.log('выполнится, если условие ложное(folse)');
+// }
+
+// const name =prompt('Скажите слово друг и заходи')
+
+// if (name === 'Друг'){
+//     alert('Привет')
+// }else{
+//     alert('Я тебя не знаю')
+// }
+
+
+// if(price >=2500) {
+//     price = price * 0.9
+// }
+
+// const found =0;
+// if (found === 0) {
+//     console.log('Ничего не найдено');
+// }else {
+//     console.log('печатаем результат поиска');
+// }
+
+//false, 0, -0, "", null, underfiend, NaN, 0n(тип bight)
+
+// const discount =0;
+// if (userStatus === 'Vip'){
+//     discount =25
+// }else if (userStatus ==='Privl') {
+//     discount =15
+// }else if (userStatus === 'nembers') {
+//     discount =5
+// }
+
+
+//тернарный оператор
+// const num =5;
+// console.log(num === 5? '5' : 'не пять');
+
+
+// const num =10;
+// let result
+
+// if(num > 10) {
+//     result = 'Число больше 10'
+// }else {
+//     result = 'Число меньше или равно 18'
+// }
+// console.log(result);
+
+
+// const num = 10;
+// const result = num > 10?'Число больше 10' : 'Число меньше или равно 10'
+
+//switch case
+switch(number) {
+    case num === 20;
+    console.log('num = 20');
+    break;
+    case num > 20:
+        console.log('num =20');
+        break;
+        default:
+}
+
+
+const arr1 = prompt ("number")
+const arr2 = prompt ("number")
+
+if () {
+    alert
+}
+else if () {
+
+}
+else if () {
+    
+}
